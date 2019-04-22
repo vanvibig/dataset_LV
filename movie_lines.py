718c2a425b84d7b66d0f9dd56c2de0d4d69b4a9e
vi <vanvibig@gmail.com>
2019-04-22 15:49:23 +0700

    add max length 10

--- movie_lines.py
+++ movie_lines.py
@@ -15,13 +15,25 @@ data_old = [l for l in data_old if len(l) > 0]
 data_new = [word_tokenize(l, format='text') for l in data_new]
 data_old = [word_tokenize(l, format='text') for l in data_old]
 
-#data_full = data_old + data_new
-#movie_lines_full_end = open('data_full_end_25k+newdata.txt', 'w', encoding='utf-8', errors='ignore')
+data_full = data_old + data_new
+movie_lines_full_end = open('data_full_end_25k+newdata.txt', 'w', encoding='utf-8', errors='ignore')
 
-data_full = data_new
-movie_lines_full_end = open('data_full_end_only+newdata.txt', 'w', encoding='utf-8', errors='ignore')
+#data_full = data_new
+#movie_lines_full_end = open('data_full_end_only+newdata.txt', 'w', encoding='utf-8', errors='ignore')
 
 num_lines = len(data_full)
+
+data_full_new = []
+
+for i in range(0,num_lines,2):
+    a = data_full[i]
+    b = data_full[i+1]
+    if len(a.split(' ')) > 10 or len(b.split(' ')) > 10:
+        continue
+    data_full_new.append(a)
+    data_full_new.append(b)
+
+num_lines = len(data_full_new)
 
 for i in range(0,num_lines):        
     movie_lines_full_end.write("L"+ str(i+1))
@@ -31,7 +43,7 @@ for i in range(0,num_lines):
     else:
         movie_lines_full_end.write("CAMERON")
     movie_lines_full_end.write(" +++$+++ ")
-    movie_lines_full_end.write(data_full[i])
+    movie_lines_full_end.write(data_full_new[i])
     movie_lines_full_end.write("\n")
 
 movie_lines_full_end.close()
